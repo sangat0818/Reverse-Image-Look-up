e33929ed26620411a93df40634c2aa0cbf7b5a28
Syangmay <35310337+Syangmay@users.noreply.github.com>
2019-03-17 13:27:08 +0530

    Update GetSearchName.py

--- GetSearchName.py
+++ GetSearchName.py
@@ -5,11 +5,11 @@ from selenium.webdriver.chrome.options import Options
 from basic import upload_files
 from imagecropper import getImage
 def searchname():
-    filename = "E:/Python/Selenium/ImagetobeSearched/image.jpg"
+    filename = "image.jpg"
     url = upload_files(filename)
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Chrome('E:/chromedriver' )
+    driver = webdriver.Chrome('E:/chromedriver',options=options )
     driver.get('https://www.google.com/searchbyimage?image_url=' + url)
     input = driver.find_element_by_name("q")
     text = input.get_attribute('value')
